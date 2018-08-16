60382b26b56d4962d2e3c57a10aa5ce79c3e5416
alexszeliga <alexszeliga@gmail.com>
2018-08-16 13:25:14 -0400

    added prompt to html page, formatted game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,7 +21,7 @@
 
 var hangmanGame = {
     author: "Alexander Szeliga",
-    unkWordArray: ["dog", "cat", "taco", "pebble"],
+    unkWordArray: ["dog", "cat", "taco", "pebble", "calisthenics", "orange", "airplane", "tomato", "crazy", "merry", "strenuous", "portend", "eminent", "immense"],
     legalLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     //the following seven are type-defined, but will be populated by the game init.
     gameWord: "",
