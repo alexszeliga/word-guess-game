e389379a073738c31fb6c1e7dccd72bf45b01337
alexszeliga <alexszeliga@gmail.com>
2018-08-16 12:52:37 -0400

    there is now a fully functional hangman game that works in the console. it tracks score, lives, re-inits after a game-over

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,9 +23,27 @@ var hangmanGame = {
     author: "Alexander Szeliga",
     unkWordArray: ["dog", "cat", "taco", "pebble"],
     legalLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    unguessedLetters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    //the following seven are type-defined, but will be populated by the game init.
+    gameWord: "",
+    gameWordArray: [],
+    unguessedLetters: [],
     guessedLetters: [],
-
+    gameBoolArray: [],
+    playerLives: 0,
+    gameOver: true,
+    // these next two are only touched when the player wins or loses.
+    playerWins: 0,
+    playerLosses: 0,
+    gameInit: function () {
+        // set/reset all game variables
+        this.playerLives = 9;
+        this.gameWord = this.randomWord(),
+            this.guessedLetters = [];
+        this.unguessedLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        this.gameWordArray = this.wordArray(this.gameWord);
+        this.gameBoolArray = this.genBoolWordArray(this.gameWord)
+        this.gameOver = false;
+    },
     letterCheck: function (userLetter) {
         // checks if a letter is in the legal letters array, which is precursor to checking if it's
         // already been guessed, which is precursor to determining if it's in the unkWord
@@ -35,7 +53,6 @@ var hangmanGame = {
             return true;
         }
     },
-
     confLetterUnguessed: function (userLetter) {
         // checks if letter is in the unguessedLetters array, returns boolean true if it is
         if (this.unguessedLetters.indexOf(userLetter) < 0) {
@@ -44,13 +61,11 @@ var hangmanGame = {
             return true;
         }
     },
-
     randomWord: function () {
         // returns a random word from the unkWordArray
         var arrInd = Math.floor(Math.random() * this.unkWordArray.length);
         return this.unkWordArray[arrInd];
     },
-
     wordArray: function (word) {
         // lets try turning a string into an array of it's composite letters
         var wordAsArray = []
@@ -59,7 +74,6 @@ var hangmanGame = {
         }
         return wordAsArray;
     },
-
     genBoolWordArray: function (word) {
         // this function will generate an array that has as many boolean items as there are letters
         // with a default value of 'false'. this will be used to control a lot of the game mechanics
@@ -70,94 +84,98 @@ var hangmanGame = {
         }
         return boolArray;
     },
-
-
-
-
+    genUnderscoreView: function (word, array) {
+        // this function generates a string that is made up of the 
+        var outputString = "";
+        for (i = 0; i < word.length; i++) {
+            if (array[i]) {
+                outputString = outputString + " " + word.charAt(i) + " ";
+            } else {
+                outputString = outputString + " _ ";
+            }
+        }
+        return outputString;
+    },
 };
-
+//
+//----------------------------------------------------------------------------------------------
+//                                       Test Function - to deprecate
+//----------------------------------------------------------------------------------------------
+//
+function consoleTest() {
+    // outputs basic game functions in console for testing
+    console.clear();
+    console.log("The game word is: " + hangmanGame.gameWord);
+    console.log("Remaining lives: " + hangmanGame.playerLives);
+    console.log(hangmanGame.genUnderscoreView(hangmanGame.gameWord, hangmanGame.gameBoolArray));
+    console.log("Player wins: " + hangmanGame.playerWins);
+    console.log("Player losses: " + hangmanGame.playerLosses);
+       
+}
 //
 //----------------------------------------------------------------------------------------------
 //                                       Main gameplay routine
 //----------------------------------------------------------------------------------------------
 //
-
-// Generate a random word from the list
-var gameWord = hangmanGame.randomWord();
-// turn that word into an array of the constituent letters
-var gameWordArray = hangmanGame.wordArray(gameWord);
-// create an array with one boolean for each letter in the gameWord
-var gameBoolArray = hangmanGame.genBoolWordArray(gameWord);
-// initialize the 'number of remaining letters' variable
-var cntRemainingLetters = gameWordArray.length;
-// init value for underscore view of word ( Taco = __c_ )
-var undView = genUnderscoreView(gameWord, gameBoolArray);
-// init value for player lives
-var userLives = 9;
-
-function genUnderscoreView(word, array) {
-    var outputString = "";
-    for (i = 0; i < word.length; i++) {
-        if (array[i]) {
-            outputString = outputString + word.charAt(i);
-        } else {
-            outputString = outputString + "_";
-        }
-    }
-    return outputString;
-}
-
-function consoleTest() {
-    console.clear();
-    console.log("The game word is: " + gameWord);
-    console.log(genUnderscoreView(gameWord, gameBoolArray));
-
-}
-console.log("The game word is: " + gameWord);
-// user presses a key
+console.log("Press any key to begin...");
+// user presses any key
 document.onkeyup = function (event) {
-
-    // set the key to a variable and turn it lower case
-    var userInput = event.key.toLowerCase();
-
-    // confirming that the userInput is a legal letter, and is unguessed
-    if (hangmanGame.letterCheck(userInput) && hangmanGame.confLetterUnguessed(userInput)) {
-
-        // we're going to add the guessed letter to the array of guessed letters and remove it from
-        // the list of unguessed letters
-        hangmanGame.guessedLetters.push(userInput);
-        hangmanGame.unguessedLetters.splice(hangmanGame.unguessedLetters.indexOf(userInput), 1);
-
-        //is the guessed letter in the game word?
-        if (gameWordArray.indexOf(userInput) < 0) {
-            consoleTest();
-            console.log("Wrong!");
-            // things that need to happen here
-            // decrement number of lives by 1
-            // add next 'body part' to hangman
-        } else {
-            // iterate through the boolean array, flipping where necessary.
-            for (i = 0; i < gameBoolArray.length; i++) {
-                if (userInput == gameWordArray[i]) {
-                    gameBoolArray[i] = true;
+    if (hangmanGame.gameOver == false) {
+        // set the key to a variable and turn it lower case
+        var userInput = event.key.toLowerCase();
+        // confirming that the userInput is a legal letter, and is unguessed
+        if (hangmanGame.letterCheck(userInput) && hangmanGame.confLetterUnguessed(userInput)) {
+            // we're going to add the guessed letter to the array of guessed letters and remove it from
+            // the list of unguessed letters
+            hangmanGame.guessedLetters.push(userInput);
+            hangmanGame.unguessedLetters.splice(hangmanGame.unguessedLetters.indexOf(userInput), 1);
+            //is the guessed letter in the game word?
+            if (hangmanGame.gameWordArray.indexOf(userInput) < 0) {
+                // if not, do this: subtract one player life
+                hangmanGame.playerLives = hangmanGame.playerLives - 1;
+                consoleTest();
+                // test if lives remain:
+                if (hangmanGame.playerLives > 0) {
+                    console.log("Wrong!");
+                } else {
+                    // add a playerLoss
+                    hangmanGame.playerLosses++;
+                    console.log("game lose, press any key to play again");
+                    hangmanGame.gameOver = true;
+                }
+            } else {
+                // iterate through the boolean array, flipping where necessary.
+                for (i = 0; i < hangmanGame.gameBoolArray.length; i++) {
+                    if (userInput == hangmanGame.gameWordArray[i]) {
+                        hangmanGame.gameBoolArray[i] = true;
+                    }
+                }
+                consoleTest();
+                // check if the word is complete:
+                if (hangmanGame.gameBoolArray.indexOf(false) == -1) {
+                    // add a win:
+                    hangmanGame.playerWins++;
+                    console.log("game win, press any key to play again");
+                    hangmanGame.gameOver = true;
+                } else {
+                    console.log("Correct");
                 }
             }
-            consoleTest();
-            console.log("Correct");
-
         }
-
-    }
-    // confirms user letter is good, but it's not on the unguessed list, which implies it's already been guessed
-    else if (hangmanGame.letterCheck(userInput) && hangmanGame.confLetterUnguessed(userInput) == false) {
+        else if (hangmanGame.letterCheck(userInput) && hangmanGame.confLetterUnguessed(userInput) == false) {
+            // confirms user letter is good, but it's not on the unguessed list, which implies it's already been guessed
+            consoleTest();
+            console.log("You've already guessed this letter");
+        }
+        else if (hangmanGame.letterCheck(userInput) == false) {
+            // illegal input
+            consoleTest();
+            console.log("Illegal input");
+        }
+    } else {
+        // this code only runs if the user presses a key while hangmanGame.gameOver = false
+        // it re-inits all the game variables with a new random word and starts the game over again.
+        hangmanGame.gameInit()
         consoleTest();
-        console.log("You've already guessed this letter");
     }
-    // illegal input
-    else if (hangmanGame.letterCheck(userInput) == false) {
-        consoleTest();
-        console.log("Illegal input");
-    }
-
-
 };
